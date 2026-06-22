ce7498d301749ce1dfb9fa1d9304af7fca5db6ea
Alicja Wojciechowska <plgvltkv@login01.helios.cyfronet.pl>
2026-06-22 17:19:12 +0200

    prompts

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -16,19 +16,11 @@ export TOKENIZERS_PARALLELISM=false
 CONTAINER="/net/software/aarch64/containers/vllm/cyfronet-gh200-vllm12.sif"
 
 apptainer exec -e --nv --bind /net:/net $CONTAINER \
-python3.13 -u $SCRATCH/unbranding/inference/run_benchmarking_new.py \
-    --benchmarking_dir $SCRATCH/unbranding/prompts \
-    --output_dir $SCRATCH/unbranding/experiments_results \
+python3.13 -u $SCRATCH/unbranding/inference/run_benchmarking.py \
+    --benchmarking_dir $SCRATCH/unbranding/prompts_by_brand2 \
+    --output_dir $SCRATCH/unbranding/experiments_results_NPO/attempt2 \
     --model_paths \
-        $SCRATCH/unbranding/models/Llama-3.1-8B-Instruct \
-        $SCRATCH/unbranding/models/Qwen2.5-3B-Instruct \
-        $SCRATCH/unbranding/models/Qwen2.5-7B-Instruct \
-        $SCRATCH/unbranding/models/Qwen2.5-14B-Instruct \
-        $SCRATCH/unbranding/models/Qwen3-4B \
-        $SCRATCH/unbranding/models/Qwen3-8B \
-        $SCRATCH/unbranding/models/Qwen3-14B \
-        $SCRATCH/unbranding/models/Qwen3-32B \
-        $SCRATCH/unbranding/models/Mistral-Small-Instruct-2409 \
-        $SCRATCH/unbranding/models/Mistral-7B-Instruct-v0.3 \
+        $SCRATCH/unbranding/models/unlearned-npo/qwen3-8b-NPO \
+        $SCRATCH/unbranding/models/qwen-32b \
     --seed 42 \
-    --judge_path $SCRATCH/unbranding/models/Qwen3-32B
+    --judge_path $SCRATCH/unbranding/models/qwen-32b
